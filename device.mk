601190e07781d46b7d9fa34591a3761f005365a9
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-06-11 15:26:30 +0200

    Remove obsolete dalvik.gc.type-precise
    Bug: 18895001
    
    original commit shamu 73eca2bc8cb85fa8312086b2ded0802fdae7e643
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- device.mk
+++ device.mk
@@ -189,10 +189,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     e2fsck
 
-# Platform specific tags
-#
-PRODUCT_TAGS += dalvik.gc.type-precise
-
 # APN list
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
