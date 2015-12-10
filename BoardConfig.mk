c085c910ed696c76d1726ea7cf49131f35a21eaa
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-12-10 09:58:45 +0100

    remove the dt version limit
    needed by the new dt format
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_KERNEL_BOOTIMG := true
 BOARD_CUSTOM_MKBOOTIMG := mkqcdtbootimg
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --dt_dir $(OUT)/dtbs --dt_version 2
+BOARD_MKBOOTIMG_ARGS += --dt_dir $(OUT)/dtbs
 
 BOARD_KERNEL_CMDLINE := androidboot.hardware=rhine androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lpj=192598
