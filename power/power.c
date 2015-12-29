24aa57f3debec93343e47b2e07bfd52fe4204510
Adam Farden <adam@farden.cz>
2015-12-29 18:48:16 +0100

    Add a simple PowerHAL
    We dropped the QCOM PowerHAL because it relies on too many
    proprietary blobs. Instead we have a kernel solution, but
    several features still depend on the presence of a PowerHAL,
    so Android is actually not happy about it.
    
    Signed-off-by: Adam Farden <adam@farden.cz>
    
    power: add some PowerHAL enhancements
    
    When the device is asleep we don't need to scale up the cpu
    frequencies so we can tune the governor to make it twice as hard
    to scale up and twice as easy to scale down.
    
    We do this by adding profiles. the 'power saving' profile can
    be used when the device is asleep, or when Android enters a low
    power mode. This profile also lowers the 'hispeed' from 1190MHz
    to 960MHz.
    
    The PowerHAL can also briefly boost frequencies to the 'hispeed'
    frequency when the user interacts with the device. The boost is
    only for 80ms but this dramatically improves responsiveness,
    including 20% quicker wake from sleep.
    
    Also fix some warnings, and the make sure we don't leave 'power
    saving' mode when waking if 'low power' mode is active.
    
    Signed-off-by: Adam Farden <adam@farden.cz>
    
    PowerHAL: AOSP does not set the governor perms
    
    Many other custom ROMs set this automatically
    
    Signed-off-by: Adam Farden <adam@farden.cz>

---
+++ power/power.c
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2016 Adam Farden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define LOG_TAG "Simple PowerHAL"
+#include <utils/Log.h>
+
+#include <hardware/hardware.h>
+#include <hardware/power.h>
+
+int sysfs_write(char *path, char *s)
+{
+    char buf[80];
+    int len;
+    int ret = 0;
+    int fd = open(path, O_WRONLY);
+
+    if (fd < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error opening %s: %s\n", path, buf);
+        return -1 ;
+    }
+
+    len = write(fd, s, strlen(s));
+    if (len < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error writing to %s: %s\n", path, buf);
+
+        ret = -1;
+    }
+
+    close(fd);
+
+    return ret;
+}
+
+static void power_init(__attribute__((unused)) struct power_module *module)
+{
+    ALOGI("Simple PowerHAL is alive!.");
+}
+
+enum {
+    PROFILE_POWER_SAVE = 0,
+    PROFILE_BALANCED,
+    PROFILE_HIGH_PERFORMANCE
+};
+
+static int current_power_profile = PROFILE_BALANCED;
+
+static void set_power_profile(int profile) {
+
+    if (profile == current_power_profile)
+        return;
+
+    switch (profile) {
+        case PROFILE_POWER_SAVE:
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay", "40000 1100000:80000 1700000:40000");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load", "90");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq", "960000");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time", "20000");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/target_loads", "85 1200000:90 1800000:70");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate", "30000");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_slack", "20000");
+            break;
+
+        case PROFILE_BALANCED:
+        case PROFILE_HIGH_PERFORMANCE:
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay", "20000 1400000:40000 1700000:20000");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load", "90");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq", "1190400");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time", "40000");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/target_loads", "85 1500000:90 1800000:70");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate", "30000");
+            sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_slack", "20000");
+            break;
+    }
+
+    current_power_profile = profile;
+}
+
+static int low_power_mode = 0;
+
+static void power_hint(__attribute__((unused)) struct power_module *module,
+                       power_hint_t hint, void *data)
+{
+    switch (hint) {
+        case POWER_HINT_VSYNC:
+            break;
+
+        case POWER_HINT_INTERACTION:
+            // When touching the screen, pressing buttons etc.
+            if (data) {
+                ALOGI("Interacting with device, boost speeds for 80000uS");
+                sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boostpulse", "1");
+            }
+            break;
+
+        case POWER_HINT_LOW_POWER:
+            // When we want to save battery.
+            if (data) {
+                ALOGI("Low power mode enabled.");
+                low_power_mode = 1;
+                set_power_profile(PROFILE_POWER_SAVE);
+            } else {
+                ALOGI("Low power mode disabled.");
+                low_power_mode = 0;
+                set_power_profile(PROFILE_BALANCED);
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+static void set_interactive(__attribute__((unused)) struct power_module *module,
+                            int on)
+{
+    // set interactive means change governor, cpufreqs etc
+    // for when device is awake and ready to be used.
+
+    if (!on) {
+        ALOGI("Device is asleep.");
+        set_power_profile(PROFILE_POWER_SAVE);
+    } else {
+        ALOGI("Device is awake.");
+        if (!low_power_mode) {
+            set_power_profile(PROFILE_BALANCED);
+        }
+    }
+}
+
+static struct hw_module_methods_t power_module_methods = {
+    .open = NULL,
+};
+
+struct power_module HAL_MODULE_INFO_SYM = {
+    .common = {
+        .tag = HARDWARE_MODULE_TAG,
+        .module_api_version = POWER_MODULE_API_VERSION_0_2,
+        .hal_api_version = HARDWARE_HAL_API_VERSION,
+        .id = POWER_HARDWARE_MODULE_ID,
+        .name = "Simple Power HAL",
+        .author = "Adam Farden",
+        .methods = &power_module_methods,
+    },
+
+    .init = power_init,
+    .powerHint = power_hint,
+    .setInteractive = set_interactive,
+};
