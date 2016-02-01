5ad815a2b792868da33f3cf67f658c4f103d87d3
Erik Castricum <erikcas1972@gmail.com>
2016-02-01 21:58:22 +0100

    rhine: re-set custom img flags
    Change-Id: I5ec96d976f6e1df9cafd412e41fd8ff6c3e3dbee
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,9 +34,9 @@ BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
 BOARD_KERNEL_BOOTIMG := true
-BOARD_CUSTOM_MKBOOTIMG := mkqcdtbootimg
+#BOARD_CUSTOM_MKBOOTIMG := mkqcdtbootimg
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --dt_dir $(OUT)/dtbs
+#BOARD_MKBOOTIMG_ARGS += --dt_dir $(OUT)/dtbs
 
 BOARD_KERNEL_CMDLINE := androidboot.hardware=rhine androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lpj=192598
