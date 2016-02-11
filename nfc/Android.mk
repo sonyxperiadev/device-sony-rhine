b5158d90d1ac0e5a6aad0ef840ba2059beaf13aa
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-11 10:52:42 +0100

    build device specific nfc.
    this change is needed after we have switched to
    androidboot.hardware=<TARGET_DEVICE>
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- nfc/Android.mk
+++ nfc/Android.mk
@@ -16,7 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := nfc.rhine
+LOCAL_MODULE := nfc.$(TARGET_DEVICE)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SRC_FILES := nfc_hw.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
