8f8c6f30c81478296b2bc09d2bf55fafa8738daa
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-09-23 13:52:15 +0200

    add BOARD_HAVE_ALTERNATE_FM
    flag is needed for the FM App to enable the build of
    of an alternate libfmjni
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -62,6 +62,7 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 
 # FM definitions for Qualcomm solution
+BOARD_HAVE_ALTERNATE_FM := true
 BOARD_HAVE_QCOM_FM := true
 TARGET_QCOM_NO_FM_FIRMWARE := true
 
